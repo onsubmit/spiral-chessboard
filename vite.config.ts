f40c0d8713b40ed3addffd44f975232ca259b39e
Andy <onsubmit@outlook.com>
2026-06-04 19:08:51 -0700

    Deploy to pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/spiral-chessboard/',
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
